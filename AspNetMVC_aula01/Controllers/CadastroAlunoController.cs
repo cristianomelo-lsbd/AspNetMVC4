5635e45f57f4898d0db389f6f52da3ec7d8df543
cristianomelo-lsbd <38224629+cristianomelo-lsbd@users.noreply.github.com>
2018-04-09 15:15:15 -0300

    Tutorial: part I and II
    This commit refers to Código Expresso tutorial:
    Part I - First steps
    Part II - Forms, Input, Submit, FormColletion, ViewBag

---
+++ AspNetMVC_aula01/Controllers/CadastroAlunoController.cs
@@ -0,0 +1,29 @@
+﻿using AspNetMVC_aula01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC_aula01.Controllers
+{
+    public class CadastroAlunoController : Controller
+    {
+        public ActionResult CadastroAluno()
+        {
+            ModAluno aluno = new ModAluno();
+            return View(aluno);
+        }
+
+        [HttpPost]
+        public ActionResult ExibeCadastroAluno(FormCollection form)
+        {
+            ViewBag.idAluno = form["idAluno"];
+            ViewBag.nome = form["nome"];
+            ViewBag.email = form["email"];
+
+            return View();
+        }
+        
+    }
+}
---
+++ AspNetMVC_aula01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetMVC_aula01.MvcApplication" Language="C#" %>
